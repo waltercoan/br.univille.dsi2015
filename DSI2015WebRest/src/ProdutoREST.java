932b2b8fa1dfd25261a9b6a292dd9a74ef1dbd71
waltercoan <walter.coan@gmail.com>
2015-11-06 18:49:39 -0200

    atualização do projeto rest para integração com android

--- DSI2015WebRest/src/ProdutoREST.java
+++ DSI2015WebRest/src/ProdutoREST.java
@@ -1,9 +1,12 @@
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
 import dao.Produto;
 import ejb.ProdutoBeanLocal;
 
@@ -20,6 +23,13 @@ public class ProdutoREST {
 	@Produces("application/json")
 	public List<Produto> getAllProdutos(){
 		return produtoBean.getTodosProdutos();
+	}
+	
+	@POST
+	@Consumes("application/json")
+	@Path("/save")
+	public void save(Produto prod){
+		produtoBean.save(prod);
 	}
 	
 	
